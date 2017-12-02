2c426609d962c6d9bc2b5850f8a72c25a2c2b68f
Linus Juhlin <juhlinus@gmail.com>
2017-12-02 14:43:42 +0900

    Removed setAlign() method call
    We don't need this since it's already centered.

--- Scenes/SecondScene.cs
+++ Scenes/SecondScene.cs
@@ -23,25 +23,25 @@ namespace Nez_Tutorial_1.Scenes
 
             Table.add(new Label("Options Screen").setFontScale(5));
 
-            Table.row().setPadTop(20).setAlign(Align.center);
+            Table.row().setPadTop(20);
 
             Table.add(new Label("This is where we can alter our options!").setFontScale(2));
 
-            Table.row().setPadTop(40).setAlign(Align.center);
+            Table.row().setPadTop(40);
 
             var playButton = Table.add(new TextButton("Next", Skin.createDefaultSkin())).setFillX().setMinHeight(30).getElement<TextButton>();
 
             playButton.onClicked += PlayButton_onClicked;
 
-            Table.row().setPadTop(40).setAlign(Align.center);
+            Table.row().setPadTop(40);
 
             Table.add(new ProgressBar(0, 1, 0.1f, false, ProgressBarStyle.create(Color.LawnGreen, Color.White)).setValue(0.45f));
 
-            Table.row().setPadTop(40).setAlign(Align.center);
+            Table.row().setPadTop(40);
 
             Table.add(new TextField("A simple Textfield!", Skin.createDefaultSkin()));
 
-            Table.row().setPadTop(40).setAlign(Align.center);
+            Table.row().setPadTop(40);
 
             Table.add(new CheckBox("Is this tutorial great?", Skin.createDefaultSkin()));
         }
